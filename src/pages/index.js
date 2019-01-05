e0eb94ec3be392d9f54695fb1572561acee7a5dd
DIonysos Dajka <diondiondion@gmail.com>
2019-01-06 00:13:28 +0100

    Display some data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,68 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import {Link, graphql} from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+function IndexPage({data: {site, allMarkdownRemark: playlists}}) {
+	const {title, audioCdnRoot} = site.siteMetadata
+	const linkPrefix = `https://${audioCdnRoot}`
+	return (
+		<Layout>
+			<h1>{title}</h1>
+			<p>Welcome to your new Gatsby site.</p>
+			<p>Playlists: {playlists.totalCount}</p>
+			<ul>
+				{playlists.edges.map(({node: playlist}) => (
+					<li key={playlist.id}>
+						{playlist.frontmatter.title} (
+						{playlist.frontmatter.tracks.length} tracks)
+						<blockquote>
+							{playlist.frontmatter.tracks.map(track => (
+								<p>
+									{track.title} (prod. by {track.producers})
+									<br />
+									<audio
+										controls
+										src={linkPrefix + track.filename}
+									/>
+								</p>
+							))}
+						</blockquote>
+					</li>
+				))}
+			</ul>
+			<Link to="/page-2/">Go to page 2</Link>
+		</Layout>
+	)
+}
 
 export default IndexPage
+
+export const query = graphql`
+	query {
+		site {
+			siteMetadata {
+				title
+				audioCdnRoot
+			}
+		}
+		allMarkdownRemark {
+			totalCount
+			edges {
+				node {
+					id
+					frontmatter {
+						title
+						artists
+						tracks {
+							title
+							artists
+							producers
+							filename
+						}
+					}
+				}
+			}
+		}
+	}
+`

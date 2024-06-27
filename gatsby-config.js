293cd69fed7912caa63610ce7e434b802494e2c0
Dionysos Dajka <diondiondion@gmail.com>
2024-06-27 16:43:52 +0200

    Remove traces of Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,12 +80,6 @@ module.exports = {
 				icon: 'src/assets/images/favicon.png', // This path is relative to the root of the site.
 			},
 		},
-		{
-			resolve: 'gatsby-plugin-netlify-cms',
-			options: {
-				modulePath: `${__dirname}/src/cms/cms.js`,
-			},
-		},
 		'gatsby-plugin-netlify', // make sure to keep it last in the array
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.app/offline
